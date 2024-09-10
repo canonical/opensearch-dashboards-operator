fab4e2081166251d91a077b7cf9ef410e0c19949
phvalguima <pedro.guimaraes@canonical.com>
2024-09-10 13:35:48 +0200

    Update literals.py

--- src/literals.py
+++ src/literals.py
@@ -4,7 +4,7 @@
 
 """Collection of global literals for the charm."""
 
-OPENSEARCH_DASHBOARDS_SNAP_REVISION = "19"
+OPENSEARCH_DASHBOARDS_SNAP_REVISION = "21"
 
 SUBSTRATE = "vm"
 CHARM_KEY = "opensearch-dashboards"
