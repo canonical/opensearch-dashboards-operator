9364fc25e787c2e7b5e6b741f21131ad72b0bd3b
Judit Novak <judit.novak@canonical.com>
2024-09-23 09:49:09 +0200

    Fix: restart if service unresponsive

--- src/literals.py
+++ src/literals.py
@@ -41,6 +41,7 @@ PEER_APP_SECRETS = [
 PEER_UNIT_SECRETS = ["ca-cert", "csr", "certificate", "private-key"]
 
 RESTART_TIMEOUT = 30
+REQUEST_TIMEOUT = 30
 
 
 # Status messages
@@ -53,25 +54,28 @@ MSG_STATUS_DB_MISSING = "Opensearch connection is missing"
 MSG_STATUS_DB_DOWN = "Opensearch service is (partially or fully) down"
 MSG_TLS_CONFIG = "Waiting for TLS to be fully configured..."
 MSG_INCOMPATIBLE_UPGRADE = "Incompatible Opensearch and Dashboards versions"
+MSG_ROLLING_RESTART = "Executing restart operation"
 
 MSG_STATUS_UNAVAIL = "Service unavailable"
 MSG_STATUS_UNHEALTHY = "Service is not in a green health state"
 MSG_STATUS_ERROR = "Service is an error state"
 MSG_STATUS_WORKLOAD_DOWN = "Workload is not alive"
 MSG_STATUS_UNKNOWN = "Workload status is not known"
+MSG_STATUS_HANGING = "Application does not respond, request hanging"
 
 MSG_APP_STATUS = [
     MSG_STATUS_DB_DOWN,
 ]
 
 MSG_UNIT_STATUS = [
+    MSG_STATUS_HANGING,
     MSG_STATUS_UNAVAIL,
     MSG_STATUS_UNHEALTHY,
     MSG_STATUS_WORKLOAD_DOWN,
     MSG_STATUS_UNKNOWN,
 ]
 
-# COS
+# COSG
 
 COS_RELATION_NAME = "cos-agent"
 COS_PORT = 9684
