921776278d701fd29076ccc27d65c11273b58437
smethnani <82812166+smethnani@users.noreply.github.com>
2025-07-17 04:08:44 +0000

    Bump snap revision

--- src/literals.py
+++ src/literals.py
@@ -4,7 +4,7 @@
 
 """Collection of global literals for the charm."""
 
-OPENSEARCH_DASHBOARDS_SNAP_REVISION = "43"
+OPENSEARCH_DASHBOARDS_SNAP_REVISION = "44"
 
 SUBSTRATE = "vm"
 CHARM_KEY = "opensearch-dashboards"
