1d38adf12c84d06ff34628d7738943b725a5d8b3
akram09 <akram.boutouchent@canonical.com>
2026-05-05 11:49:12 +0200

    patch: Fix message literal not found

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -11,7 +11,6 @@ import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
 
-from single_kernel_opensearch_dashboards.literals import MSG_STATUS_DB_UNHEALTHY
 
 from .helpers import (
     CONFIG_OPTS,
@@ -348,7 +347,7 @@ async def test_dashboard_status_changes(ops_test: OpsTest):
     assert await check_full_status(
         ops_test,
         status="blocked",
-        status_msg=MSG_STATUS_DB_UNHEALTHY,
+        status_msg="The OpenSearch service health is red",
     )
 
 
